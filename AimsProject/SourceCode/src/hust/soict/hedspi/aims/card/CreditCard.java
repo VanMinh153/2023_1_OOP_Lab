1c66f83a335e12e47f0f9ecef311b679c5f33c3b
Admin <minhnguyenvh.703@gmail.com>
2023-12-09 18:13:21 +0700

    git commit -m "AIMS"

---
+++ AimsProject/SourceCode/src/hust/soict/hedspi/aims/card/CreditCard.java
@@ -0,0 +1,36 @@
+package hust.soict.hedspi.aims.card;
+
+public class CreditCard {
+    private String card_number;
+    private String cardholder_name;
+    private String expiration_date;
+    private int CVV;
+    private float current_balance;
+    private float credit_limit;
+    
+    // Getter
+    public String getCard_number() {
+        return card_number;
+    }
+
+    public String getCardholder_name() {
+        return cardholder_name;
+    }
+
+    public String getExpiration_date() {
+        return expiration_date;
+    }
+
+    public int getCVV() {
+        return CVV;
+    }
+
+    public float getCurrent_balance() {
+        return current_balance;
+    }
+
+    public float getCredit_limit() {
+        return credit_limit;
+    }
+    
+}
