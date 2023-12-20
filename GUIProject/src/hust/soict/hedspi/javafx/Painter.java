ecd0c73f8f51673a6e5a7dde2221b961624efbc7
Admin <minhnguyenvh.703@gmail.com>
2023-12-20 19:09:26 +0700

    GUI Project

--- GUIProject/src/hust/soict/hedspi/javafx/Painter.java
+++ GUIProject/src/hust/soict/hedspi/javafx/Painter.java
@@ -1,28 +1,34 @@
-
+ 
 package hust.soict.hedspi.javafx;
-
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-
 public class Painter extends Application{
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/Painter.fxml") );
+        Parent root = FXMLLoader.load(getClass().getResource("Painter.fxml"));
         
         Scene scene = new Scene(root);
         stage.setTitle("Painter");
         stage.setScene(scene);
         stage.show();
-        
     }
     
     public static void main(String[] args) {
         launch(args);
     }
     
+    
 }
+/*
+
+*/
--- GUIProject/src/hust/soict/hedspi/javafx/PainterController.java
+++ GUIProject/src/hust/soict/hedspi/javafx/PainterController.java
@@ -1,27 +1,32 @@
 package hust.soict.hedspi.javafx;
 
-import javafx.event.ActionEvent;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseEvent;
 import javafx.fxml.FXML;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
-import static javafx.scene.paint.Color.color;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
-
-public class PainterController {
+/**
+ * FXML Controller class
+ *
+ * @author quyetdaika
+ */
+public class PainterController{
     @FXML
     private Pane drawingAreaPane;
-    
     @FXML
+    private VBox painterVbox;
+    
     void clearButtonPressed(ActionEvent event) {
         drawingAreaPane.getChildren().clear();
     }
     
-    @FXML
     void drawingAreaMouseDragged(MouseEvent event) {
         Circle newCircle = new Circle(event.getX(), event.getY(), 4, Color.BLACK);
         drawingAreaPane.getChildren().add(newCircle);
     }
-    
-    
+
 }
